cecfdbad0c5e90220f5e8d3875fcaa59fda38424
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-15 20:46:23 +0100

    create a new FeedItem Model

---
+++ FeedFeature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedFeature
+//
+//  Created by Srinivasan Rajendran on 2020-12-15.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
