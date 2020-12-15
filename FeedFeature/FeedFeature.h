8f2e533d545af6ee07293d9515e98e99fd5e7ab4
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-15 20:40:16 +0100

    Initial Commit

---
+++ FeedFeature/FeedFeature.h
@@ -0,0 +1,18 @@
+//
+//  FeedFeature.h
+//  FeedFeature
+//
+//  Created by Srinivasan Rajendran on 2020-12-15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedFeature.
+FOUNDATION_EXPORT double FeedFeatureVersionNumber;
+
+//! Project version string for FeedFeature.
+FOUNDATION_EXPORT const unsigned char FeedFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedFeature/PublicHeader.h>
+
+
