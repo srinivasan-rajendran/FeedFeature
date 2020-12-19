cac48c37cbf63e2374ab9243be515146257d78ff
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-19 12:49:38 +0100

    move HttpClient to its own file

---
+++ FeedFeature/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPClient.swift
+//  FeedFeature
+//
+//  Created by Srinivasan Rajendran on 2020-12-19.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
