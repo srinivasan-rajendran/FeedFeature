ac9e37695d00a0b6e08dff32022924b925e3c56a
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-21 21:15:26 +0100

    organise files into folders and move URLSessionHTTPClient into its own class and make access specifiers of the class as public

---
+++ FeedFeature/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,30 @@
+//
+//  URLSessionHTTPClient.swift
+//  FeedFeature
+//
+//  Created by Srinivasan Rajendran on 2020-12-21.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    private struct UnexpectedValuesRepresentation: Error {}
+
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
