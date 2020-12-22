faf72679d5331d5793743b273021f3948a9ffa60
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-22 19:51:55 +0100

    Save Command requests cache deletion

--- FeedFeatureTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ FeedFeatureTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,25 @@
 //
 
 import XCTest
+import FeedFeature
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
     let store: FeedStore
+
     init(store: FeedStore) {
         self.store = store
+    }
+
+    func save(items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
@@ -25,6 +35,23 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        sut.save(items: [uniqueItems(), uniqueItems()])
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+
+    // MARK: Helpers
+
+    private func uniqueItems() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "https://example.com")!
     }
 
 }
