bc957a777cb096af079844e6416fbfc927e1e41f
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-22 13:29:39 +0100

    Rename test class to reflect its feature

--- FeedFeatureTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ FeedFeatureTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import FeedFeature
 
-class RemoteFeedLoaderTests: XCTestCase {
+class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
