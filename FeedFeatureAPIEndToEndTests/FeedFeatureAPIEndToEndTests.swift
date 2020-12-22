22ef0fdd1646d4cc4db6e05b41cc6fc22d15706e
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-22 13:19:07 +0100

    use ephemeral state configuration for URL Session to make sure it does not cache the response.

--- FeedFeatureAPIEndToEndTests/FeedFeatureAPIEndToEndTests.swift
+++ FeedFeatureAPIEndToEndTests/FeedFeatureAPIEndToEndTests.swift
@@ -34,7 +34,7 @@ class FeedFeatureAPIEndToEndTests: XCTestCase {
 
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let remoteFeedLoader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(remoteFeedLoader, file: file, line: line)
