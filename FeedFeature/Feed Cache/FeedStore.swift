a0ba1bea5970dbccf7f7c2178cd1010f249eabe8
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-27 15:25:34 +0100

    add comments to let clients know that completion blocks can be invoked in any thread.

--- FeedFeature/Feed Cache/FeedStore.swift
+++ FeedFeature/Feed Cache/FeedStore.swift
@@ -18,7 +18,15 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
 
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insertFeed(items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieveFeed(completion: @escaping RetrievalCompletion)
 }
