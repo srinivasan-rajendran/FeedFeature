ad83817ecf80674909158975712e972b112c2ccf
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2020-12-28 23:15:41 +0100

    Simply representation of insertion and deletion result

--- FeedFeature/Feed Cache/FeedStore.swift
+++ FeedFeature/Feed Cache/FeedStore.swift
@@ -11,10 +11,13 @@ public typealias CachedFeed = (feed: [LocalFeedItem], timestamp: Date)
 
 public protocol FeedStore {
 
-    typealias RetrievalResult = Result<CachedFeed?, Error>
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
 
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
+
+    typealias RetrievalResult = Result<CachedFeed?, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void
 
     /// The completion handler can be invoked in any thread.
